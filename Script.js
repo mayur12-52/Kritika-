74eb5768d4315d20fff15cf7e57a3a1c1e336134
mayur katore <katoremayur9@gmail.com>
2025-08-02 08:08:11 +0530

    Update and rename Script.jsd to Script.js

---
+++ Script.js
@@ -0,0 +1,4 @@
+document.getElementById('changeImageBtn').addEventListener('click', function() {
+    const image = document.getElementById('myImage');
+    image.src = 'https://www.google.com/imgres?q=laser%20scarecrows%20in%20agriculture&imgurl=https%3A%2F%2Fvegetablegrowersnews.com%2Fwp-content%2Fuploads%2F2022%2F05%2FLaser-scarecrows-in-sweet-corn-field-1.png&imgrefurl=https%3A%2F%2Fvegetablegrowersnews.com%2Farticle%2Flaser-scarecrows-studied-to-deter-birds-from-fields%2F&docid=oIODADsyZqV_FM&tbnid=K34pWgW673Tp6M&vet=12ahUKEwjEoYLvi-uOAxVa3jgGHdctBAYQM3oECDgQAA..i&w=660&h=330&hcb=2&ved=2ahUKEwjEoYLvi-uOAxVa3jgGHdctBAYQM3oECDgQAA'; // Change to a new image
+});
